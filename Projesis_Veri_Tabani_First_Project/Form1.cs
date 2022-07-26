b05cf1b8faa00c63f3a68031a41d3ebd4135e50d
ArdaOdabasi <61662021+ArdaOdabasi@users.noreply.github.com>
2022-07-26 20:46:41 +0300

    Successfully Completed
    The project was completed successfully.

---
+++ Projesis_Veri_Tabani_First_Project/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projesis_Veri_Tabani_First_Project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InternDal internDal = new InternDal();
+
+            DgvInterns.DataSource = internDal.GetAll();
+        }
+    }
+}
---
+++ Projesis_Veri_Tabani_First_Project/InternDal.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projesis_Veri_Tabani_First_Project
+{
+    public class InternDal
+    {
+        public DataTable GetAll()
+        {
+            SqlConnection sqlConnection = new SqlConnection(@"server=(localdb)\mssqllocaldb;initial catalog=Internship;integrated security=true");
+
+            if (sqlConnection.State == ConnectionState.Closed)
+            {
+                sqlConnection.Open();
+            }
+
+            SqlCommand sqlCommand = new SqlCommand("Select * from Intern", sqlConnection);
+
+            SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+
+            DataTable dataTable = new DataTable();
+            dataTable.Load(sqlDataReader);
+            sqlDataReader.Close();
+            sqlConnection.Close();
+
+            return dataTable;
+        }
+    }
+}
